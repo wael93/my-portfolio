d2a4cebc48ca1a62f7598cca55262c996298908d
z3by <zoaby.am@gmail.com>
2018-10-13 22:29:26 +0300

    visits counter

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+let visits = 0;
 
 app.use(express.static(__dirname + "/build"));
+
 app.get("/", (req, res) => {
+  visits++;
   res.sendFile(__dirname + "/build/index.html");
+});
+
+app.get("/visits", (req, res) => {
+  res.send(visits);
 });
 
 app.get("*", (req, res) => {
