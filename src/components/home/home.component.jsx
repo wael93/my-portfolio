4c7e5a646ba94225947af4cb244a309772bc2c49
z3by <zoaby.am@gmail.com>
2018-12-11 00:12:47 +0200

    setup the main screen movies style

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,63 +1,32 @@
 import React, { Component } from "react";
 import "./home.style.css";
-import Header from "../header/header.component";
-import Social from "../social/social.component";
-
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    window.onscroll = this.onScroll;
   }
-
-  onScroll = e => {
-    this.translateHeaderImg(e);
-
-    if (window.innerHeight <= window.pageYOffset) {
-      this.showDownloadBtn();
-    } else {
-      this.hideDownloadBtn();
-    }
-  };
-
-  showDownloadBtn = () => {
-    document.querySelector(".drop").classList.add("show-btn");
-  };
-
-  hideDownloadBtn = () => {
-    if (document.querySelector(".drop").classList.contains("show-btn")) {
-      document.querySelector(".drop").classList.remove("show-btn");
-    }
-  };
-
-  translateHeaderImg = e => {
-    document.querySelector(".overlay-down").style.backgroundPositionY =
-      window.scrollY / 20 + "px";
-    document.querySelector(".overlay-down").style.filter =
-      "grayscale(" + window.scrollY / 5 + "%)";
-
-    document.querySelector(".ver-line").style.height =
-      window.scrollY * 0.7 + "px";
-  };
 
   render() {
     return (
-      <div className="home">
-        <Header />
-        <Social />
-        <div className="line-drop">
-          <div className="ver-line" />
-          <div className="drop">
-            <a
-              className="download-btn-text"
-              target="_blanck"
-              href="https://drive.google.com/file/d/1ZkdFAaNuawv8VD7Tx9Wbt5Sc0AmYY91Z/view?usp=sharing"
-            >
-              download my resume
-            </a>
+      <React.Fragment>
+        <div className="home fade-slow">
+          <img
+            className="overlay"
+            src="https://ak9.picdn.net/shutterstock/videos/1014503369/thumb/1.jpg"
+            alt=""
+          />
+          <div className="grow-slow lighting">
+            <h1 className="title">I AM</h1>
+            <h1 className="title">AHMAD MOSTAFA AL-ZOUBI</h1>
+            <h4 className="subtitle">Full Stack Developer</h4>
           </div>
+          <img
+            className="down-arrow"
+            src="https://www.crucial.com.au/wp-content/uploads/2014/12/down-arrow-white.png"
+            alt=""
+          />
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
