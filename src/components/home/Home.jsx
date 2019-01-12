70cdce77a788ae62a04b05bd44d3133c742c1d9d
z3by <zoaby.am@gmail.com>
2019-01-13 00:44:47 +0300

    init theme

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>hello</h1>
+      <div className="home">
+        <div className="outlined home__text-wrapper">
+          <h1 className="text-display1">Hello</h1>
+          <h2 className="text-display2">I'm <span className=""> Ahmad Mostafa </span></h2>
+          <h2 className="text-display2">Full Stack Developer</h2>
+        </div>
       </div>
     )
   }
