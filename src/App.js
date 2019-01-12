80e5da53da31d09d7969089f6058e795ce02d4a7
z3by <zoaby.am@gmail.com>
2019-01-12 22:39:41 +0300

    init react app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import Home from "./components/home/home.component";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    AOS.init({
-      // Global settings
-      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-      startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-      initClassName: "aos-init", // class applied after initialization
-      animatedClassName: "aos-animate", // class applied on animation
-      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-      // Settings that can be overriden on per-element basis, by `data-aos-*` attributes:
-      offset: 320, // offset (in px) from the original trigger point
-      delay: 100, // values from 0 to 3000, with step 50ms
-      duration: 1400, // values from 0 to 3000, with step 50ms
-      easing: "ease", // default easing for AOS animations
-      once: false, // whether animation should happen only once - while scrolling down
-      mirror: false, // whether elements should animate out while scrolling past them
-      anchorPlacement: "top-bottom" // defines which position of the element regarding to window should trigger the animation
-    });
-  }
-
   render() {
     return (
       <div className="App">
-        <Home />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
